739c0a0a40621236041e138783f91e9b5550f9d4
Jongsin Park <pjsin865@nexell.co.kr>
2016-01-07 15:38:53 +0900

    -- libimobiledevice : remvoe libxml2

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
-LOCAL_PATH := $(call my-dir)
+ifeq ($(BOARD_USES_IOS_IAP_TETHERING),true)
 
+LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/android/jni/libiconv.mk \
-	$(LOCAL_PATH)/android/jni/libxml2.mk \
 	$(LOCAL_PATH)/android/jni/libplist.mk \
 	$(LOCAL_PATH)/android/jni/libusbmuxd.mk \
 	$(LOCAL_PATH)/android/jni/libusb.mk \
@@ -9,3 +9,4 @@ include $(LOCAL_PATH)/android/jni/libiconv.mk \
 	$(LOCAL_PATH)/android/jni/usbmuxd.mk \
 	$(LOCAL_PATH)/ipod_mgr_service/ipod_mgr.mk
 
+endif
